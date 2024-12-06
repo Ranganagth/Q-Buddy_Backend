1572e4582a28d2717a65d7de8718f114b20ec50e
Ranganath <87424392+Ranganagth@users.noreply.github.com>
2024-12-06 18:55:30 +0530

    Update main.ts
    updated frontend url

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   // Enable CORS with specific origin
   app.enableCors({
     // origin: process.env.ALLOWED_ORIGINS.split(','),
-    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:5501', 'http://127.0.0.1:8080', 'http://localhost:4200', 'http://notify2you.xtgem.com/'],
+    origin: ['http://notify2you.xtgem.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you need to send cookies, etc.
   });
