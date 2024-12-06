fcdd4b6f04993d4ab788acc38cd7de21f3ca18f1
Ranganath <87424392+Ranganagth@users.noreply.github.com>
2024-12-06 17:56:52 +0530

    feat: Push Notification

---
+++ src/config/configuration.ts
@@ -0,0 +1,5 @@
+export const webPushConfig = {
+  publicKey: 'BJ_RCJTMyRUtQTDeIHDRS2m9JefdxS1lA_Ryo3MPhJ89I3yyG1ts9VdrIWuxuj2EAHwt70h43WqROsbF7qb53mA',
+  privateKey: 'AfjNKSeJPBDCswm-W_TxOG6jbpfg9XIU8YbqdjcVhy0',
+  email: 'mailto:nevil.mali23799@gmail.com',
+};
---
+++ src/notifications/notifications.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Body, Get, BadRequestException } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private readonly notificationsService: NotificationsService) { }
+
+  @Post('/subscribe')
+  addSubscription(@Body() subscription: any) {
+    if (!subscription || !subscription.endpoint) {
+      throw new BadRequestException('Invalid subscription object. Endpoint is required.');
+    }
+    this.notificationsService.addSubscription(subscription); // Pass the subscription object to the service
+    return { message: 'Subscription added successfully.' };
+  }
+
+
+
+  @Post('/broadcast')
+  async broadcastNotification(@Body('payload') payload: string) {
+    await this.notificationsService.sendNotificationToAll(payload);
+    return { message: 'Notifications sent to all subscribers.' };
+  }
+
+  @Get('/subscriptions')
+  getSubscriptions() {
+    return this.notificationsService.getSubscriptions();
+  }
+}
